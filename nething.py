daa03ac37b948915fada8df79c76dd7d57039a2c
jogil-rivera <jogilrivera@gmail.com>
2023-03-18 10:52:40 -0500

    nething

---
+++ nething.py
@@ -0,0 +1,14 @@
+print('phasers set to stun.')
+
+class smugmoney:
+        def __init__(self, name):
+            self.name = name
+
+p1 = smugmoney('rlandoo loomb')
+print(p1.name)
+
+def hi_bb(x):
+    print('mama, mia')
+    print('x')
+
+hi_bb('here it goes, again')
