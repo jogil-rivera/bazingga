2cbeb258dadba80a878e80539fd988183e8ab2b4
jogil-rivera <jogilrivera@gmail.com>
2023-03-18 12:08:06 -0500

    nething

--- ne-thing.py
+++ ne-thing.py
@@ -1,16 +1,20 @@
-print('phasers set to stun.')
+import PySimpleGUI as sg
 
-class smugmoney:
-    my_mo = 'ten toes'
-    def __init__(self, name):
-            self.name = name
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('first name'), sg.InputText()],
+            [sg.Text('last name'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Exit')]]
 
+# Create the Window
+window = sg.Window('Window Title', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('ID:', '<', values[0], '>', ', ', '<', values[1], '>', sep="")
+    # print(event)
+    # print(values)
 
-p1 = smugmoney('rlandoo loomb')
-print(p1.name)
-
-def hi_bb(x):
-    print('mama, mia')
-    print(x)
-
-hi_bb('here it goes, again')
+window.close()
