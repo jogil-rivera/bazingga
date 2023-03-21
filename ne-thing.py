1941c6fe3cc97fb653209e87be9e83055739d64e
jogil-rivera <jogilrivera@gmail.com>
2023-03-21 09:57:34 -0500

    nething

--- ne-thing.py
+++ ne-thing.py
@@ -1,20 +1,25 @@
-import PySimpleGUI as sg
+# import PySimpleGUI as sg
+#
+# sg.theme('DarkAmber')   # Add a touch of color
+# # All the stuff inside your window.
+# layout = [  [sg.Text('first name'), sg.InputText()],
+#             [sg.Text('last name'), sg.InputText()],
+#             [sg.Button('Ok'), sg.Button('Exit')]]
+#
+# # Create the Window
+# window = sg.Window('Window Title', layout)
+# # Event Loop to process "events" and get the "values" of the inputs
+# while True:
+#     event, values = window.read()
+#     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+#         break
+#     print('ID:', '<', values[0], '>', ', ', '<', values[1], '>', sep="")
+#     # print(event)
+#     # print(values)
+#
+# window.close()
 
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('first name'), sg.InputText()],
-            [sg.Text('last name'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Exit')]]
+import random
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-    print('ID:', '<', values[0], '>', ', ', '<', values[1], '>', sep="")
-    # print(event)
-    # print(values)
-
-window.close()
+num = random.randrange(1,3)
+print(num)
